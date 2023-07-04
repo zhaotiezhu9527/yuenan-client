ece040ffbe38931c360ecc0a8c022499772ab915
water <watershui122@gmail.com>
2023-07-04 12:49:02 +0700

    接口，下面导航翻译

--- yuenan-ui/plugins/base.js
+++ yuenan-ui/plugins/base.js
@@ -28,8 +28,8 @@ export const loading = (title) => {
 };
 const PATH_URL =
   process.env.NODE_ENV === "development"
-    ? "http://192.168.4.85:9522/" //本地测试
-    // ? "http://ymqtapi.juhai.xyz/ym-qtapi-test/"" //测试
+    // ? "http://192.168.4.85:9522/" //本地测试
+    ? "http://yuenanqt.juhai.xyz/yuenan-qtapi-test/" //测试
     // : "/api/";//h5打包地址
     // :  "https://www.anke9988.com/DISOZzbHUGxkbPh2/"; //杨杨a1 安科app打包地址
 // : "/DISOZzbHUGxkbPh2/"; //杨杨a1 安科h5打包地址
