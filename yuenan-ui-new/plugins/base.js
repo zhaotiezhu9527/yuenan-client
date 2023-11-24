9e24574f6995a5e36e49db22762fcbc3ffdbd3bb
water <watershui122@gmail.com>
2023-11-24 14:49:04 +0700

    109版本域名

--- yuenan-ui-new/plugins/base.js
+++ yuenan-ui-new/plugins/base.js
@@ -31,7 +31,7 @@ const PATH_URL =
   process.env.NODE_ENV === "development"
     // ? "http://192.168.4.85:9522/" //本地测试
     ? "http://yuenanqt.juhai.xyz/yuenan-qtapi-test/" //测试
-	  : "https://www.acv8888.top/xiaoai-qtapi-prod/"; //app打包地址测试
+	  : "https://www.acv6666.top/xiaoai-qtapi-prod/"; //app打包地址测试
 	  // : "/xiaoai-qtapi-prod/"; 
 	  // : "/yuenan-qtapi-test/"; 
 
